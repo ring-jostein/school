200d4437a9dfb752e1559b70d3d086f9e626cdcf
Jostein Ring <ring.jostein@gmail.com>
2019-02-12 17:54:11 +0100

    Added existing project files

---
+++ Oblig 1/O1/O1/O1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace O1
+{
+    static class O1
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
